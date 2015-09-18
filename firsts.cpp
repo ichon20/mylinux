e37144c00ecf5f67b8cfcfdfbb3cc7fd0cbae105
dchung <dchung@yahoo.com>
2015-09-17 20:00:22 -0700

    Added new cpp file

---
+++ firsts.cpp
@@ -0,0 +1,22 @@
+// firsts.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+void main() {
+    int val1, val2;
+	val1 = 1;
+	val2 = 2;
+	const int* pInt;
+	int const* pConst;
+
+	pInt = &val1;
+	pConst=&val2;
+
+	printf("Hello test\n");
+	*pInt = 10;
+	*pConst=val1;
+}
